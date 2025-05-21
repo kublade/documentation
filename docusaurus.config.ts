e1f35e55559ed8648d4228505b405ef8d2d14e43
Marcel Menk <marcel.menk@vmmx.io>
2025-05-21 15:26:18 +0200

    chore: move documentation link to right sidebar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -57,7 +57,7 @@ const config: Config = {
         {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
-          position: 'left',
+          position: 'right',
           label: 'Documentation',
         },
         {
